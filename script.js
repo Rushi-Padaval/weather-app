87a296a16b11d5b7ccb132343f26bbe4787490e9
Rushi-Padaval <rushikeshpadaval@outlook.com>
2024-07-05 04:18:55 +0530

    asynch and dom manipulation

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 const apiKey = "d6270b1dab7081cdb90310a0d2194692";
-const apiUrl= "https://api.openweathermap.org/data/2.5/weather?&units=metrics";
+const apiUrl= `https://api.openweathermap.org/data/2.5/weather?&units=metrics&q=mumbai`;
+
+async function checkWeather() 
+{
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    let data = await response.json();
+
+    console.log(data);
+    
+}
+checkWeather() 
+
+let data = document.querySelector(data)
