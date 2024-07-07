e26af05d3cdb5f50a4d350549e708987f61e5303
Rushi-Padaval <rushikeshpadaval@outlook.com>
2024-07-08 00:14:55 +0530

    Project Completed But  Under Updates

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ async function checkWeather(city) {
         document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
         document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
         document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
-console.log(data)  
+
     } else {
         alert("City not found!");
     }
@@ -21,6 +21,19 @@ console.log(data)
         weatherIcon.src = "images/clouds.png";
         
     }
+    else if(data.weather[0].main == "Clear"){
+        weatherIcon.src = "images/clear.png";
+    }
+    else if (data.weather[0].main == "Rain"){
+        weatherIcon.src = "images/rain.png";
+    }
+    else if (data.weather [0].main == "Drizzle"){
+        weatherIcon.src = "images/drizzle.png";
+    }
+    else if (data.weather [0].main == "Mist"){
+        weatherIcon.src = "images/mist.png";
+    }
+    document.querySelector(".weather").style.display = "block";
 }
 
 searchBtn.addEventListener("click", () => {
